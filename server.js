2febdfdea1141ffca13658ada8bed933424806e0
shiela-gragasin <22-63946@g.batstate-u.edu.ph>
2025-12-01 14:40:34 +0000

    update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// Serve static files from "public" folder
+// Serve static files (CSS, images) from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route for your HTML page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/layover.html"));
 });
 
+// ---- PLACE PORT LISTENER AT THE END ----
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
